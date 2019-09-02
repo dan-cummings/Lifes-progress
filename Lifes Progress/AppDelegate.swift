27c298d1b7405d3b159bd3b1e0c026e70c4524f4
Dan <Dan.cummings@dynatrace.com>
2019-09-02 15:16:18 -0700

    Initial Commit

---
+++ Lifes Progress/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Lifes Progress
+//
+//  Created by Cummings, Dan on 9/2/19.
+//  Copyright © 2019 Cummings, Dan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Lifes Progress/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Lifes Progress
+//
+//  Created by Cummings, Dan on 9/2/19.
+//  Copyright © 2019 Cummings, Dan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
